e4b1089045b20ea552bb1406daa8232235dfaa0e
idk <hankhill19580@gmail.com>
2022-09-07 04:35:14 -0400

    update DESCRIPTION.html

--- release.sh
+++ release.sh
@@ -2,7 +2,6 @@
 
 . ./config.sh
 ./clean.sh
-./javadoc.sh
 NUMLINE=`grep release.number build.xml | head -n 1`
 sed -i "s|$NUMLINE|        <property name=\"release.number\" value=\"$GITHUB_TAG\" />|g" build.xml
 edgar && git push --all
